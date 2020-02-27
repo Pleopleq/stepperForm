f3d120642ab0fac3955a01fe69665fa6029c7f21
Felix Anducho <felix.anducho74@gmail.com>
2020-02-26 19:18:15 -0600

    Add an email validate

--- app.js
+++ app.js
@@ -4,23 +4,25 @@ class Stepper {
         this.nextButton = document.querySelector('.blockForm__next');
         this.backButton = document.querySelector('.blockForm__back');
         this.submit = document.querySelector('.finishedForm');
-        this.input = document.querySelectorAll('.blockForm__input');
+        this.inputEmail = document.querySelector('.blockForm__email');
+        this.inputPassword = document.querySelector('.blockForm__password');
         this.buttonsDiv = document.querySelector('.blockForm__buttons');
         this.currentTab = 0;
     }
 
     showNextInput(tab){
         let step = this.step;
-        let backButton = this.backButton;
-        let nextButton = this.nextButton;
-
+        const backButton = this.backButton;
+        const nextButton = this.nextButton;
+      
         step[tab].style.display = "block";
         if (tab == 0) {
           backButton.style.display = "none";
         } else {
           backButton.style.display = "inline";
         }
-        if (tab == (step.length - 1)) {
+
+        if (tab == (step.length - 2)) {
           nextButton.innerHTML = "Submit";
         } else {
           nextButton.innerHTML = "Next";
@@ -28,23 +30,31 @@ class Stepper {
     }
     nextBack(field) {
         let step = this.step;
-        let input = this.input;
-        
-        if(input[this.currentTab].value === ''){ 
-                this.showAlert("Please, fill all the inputs");
-                return
-        } else {  
+        const submitForm = this.submit;
+        const nextButton = this.nextButton;
+        const backButton = this.backButton;
         // Hide the current tab:
         step[this.currentTab].style.display = "none";  
         // Increase or decrease the current tab by 1:
-        this.currentTab = this.currentTab + field;
-        // if you have reached the end of the form... :
-        if (this.currentTab >= step.length) {
-          //...the form gets submitted:
-          this.submit.submit();
+        if(this.inputEmail.value === ''){
+          this.showAlert("Please enter an Email!");
+        } else {
+          console.log(this.currentTab + ' currentTab')
+          console.log(field + ' field ')
+          this.currentTab = this.currentTab + field ;
         }
+        
         this.showNextInput(this.currentTab)
-        }
+        // if you have reached the end of the form... :
+          if (this.currentTab >= step.length -1) {
+            //...the form gets submitted and the buttons are hidden
+            nextButton.style.display = "none";
+            backButton.style.display = "none";
+            setTimeout(  () => {
+              submitForm.submit();
+            },500)
+          }
+
     }
 
     showAlert(text){
@@ -57,7 +67,6 @@ class Stepper {
 		}, 2000);
         parentEl.appendChild(el);
     }
-
 }
 
 
@@ -66,8 +75,6 @@ class Stepper {
 const stepper = new Stepper;
 
 let currentTab = stepper.currentTab;
-
-console.log(currentTab)
 
 stepper.showNextInput(currentTab);
 
